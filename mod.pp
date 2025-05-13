6709294175591d4d5dc0325099416f92ca068785
Priyanka Chatterjee <priyanka.chatterjee@turbot.com>
2025-05-13 17:31:13 +0530

    Update Azure Perimeter mod description and README with detailed usage instructions and installation steps

--- mod.pp
+++ mod.pp
@@ -1,15 +1,15 @@
 mod "azure_perimeter" {
   # Hub metadata
   title         = "Azure Perimeter"
-  description   = "Is your Azure perimeter secure? Powerpipe and Steampipe can help you check your Azure subscriptions for public resources, resources shared with untrusted tenants, insecure network configurations and more."
+  description   = "Run security controls across all your Azure subscriptions to look for resources that are publicly accessible, shared with untrusted accounts, have insecure network configurations, and more across all your Azure subscriptions using Powerpipe and Steampipe."
   color         = "#0089D6"
   documentation = file("./docs/index.md")
   icon          = "/images/mods/turbot/azure-perimeter.svg"
-  categories    = ["azure", "compliance", "perimeter", "security"]
+  categories    = ["azure", "public cloud", "perimeter", "security"]
 
   opengraph {
     title       = "Powerpipe Mod for Azure Perimeter"
-    description = "Is your Azure perimeter secure? Powerpipe and Steampipe can help you check your Azure subscriptions for public resources, resources shared with untrusted tenants, insecure network configurations and more."
+    description = "Run security controls across all your Azure subscriptions to look for resources that are publicly accessible, shared with untrusted accounts, have insecure network configurations, and more across all your Azure subscriptions using Powerpipe and Steampipe."
     image       = "/images/mods/turbot/azure-perimeter-social-graphic.png"
   }
 
